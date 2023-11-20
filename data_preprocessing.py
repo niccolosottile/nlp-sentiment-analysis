69f792d1c9fdd297e9a441146ae54b25f16b1421
= <niccolosottile@outlook.com>
2023-11-20 17:37:34 +0000

    Added function to run preprocessing on all sets of features

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,8 @@
+#%%
 import os
 import spacy
 from spacy.cli import download
+from nltk.stem.lancaster import LancasterStemmer
 
 download("en_core_web_sm")
 nlp = spacy.load("en_core_web_sm")
@@ -20,34 +22,52 @@ def read_reviews(folder_path):
                 # Assign labels given extracted star rating
                 label = 1 if star_rating >= 7 else 0
 
-                # Apply preprocessing on review content
-                tokens = preprocess_review(content)
-
-                # Assign each review a dictionary of label and preprocessed content
-                reviews[filename] = {'label': label, 'content': tokens}
+                # Assign each review a dictionary of label and content
+                reviews[filename] = {'label': label, 'content': content}
 
     return reviews
 
-def preprocess_review(content):
-    # Process content using Spacy NLP pipeline
-    # Includes tokenization, stopwords flag, lemmatized token
-    doc = nlp(content)
-
-    # Apply tokenization, remove stopwords, lemmatization
-    tokens = [token.lemma_ for token in doc if not token.is_stop]
-    
-    return tokens
-
 # Read positive and negative reviews
 pos_reviews = read_reviews('../data/pos') 
-neg_reviews = read_reviews('../data/neg')  
+neg_reviews = read_reviews('../data/neg') 
+
+#%%
+def preprocess_reviews(reviews, stopwords = False, punctuation = False, stemming = False, lemmatization = False):
+    preprocessed_reviews = {}
+
+    for filename in reviews.keys():
+        content = reviews[filename]['content']
+
+        # Process content using Spacy NLP pipeline
+        doc = nlp(content)
+
+        if stemming:
+            # Lemmatization not available in Spacy, using NLTK's LancasterStemmer
+            st = LancasterStemmer()
+            # Applying stemming
+            tokens = [st.stem(token.text.lower()) for token in doc if (stopwords and not token.is_stop) and (punctuation and not token.is_punct)]
+        elif lemmatization:
+            # Applying lemmatization
+            tokens = [token.lemma_.lower() for token in doc if (stopwords and not token.is_stop) and (punctuation and not token.is_punct)]
+        else:
+            tokens = [token.text.lower() for token in doc if (stopwords and not token.is_stop) and (punctuation and not token.is_punct)]
+
+        preprocessed_reviews[filename] = {'label': reviews[filename]['label'], 'content': tokens}
+    
+    return preprocessed_reviews 
+
+# Preprocess data
+preprocessed_pos = preprocess_reviews(pos_reviews, True, True, False, True)
+preprocessed_neg = preprocess_reviews(neg_reviews, True, True, False, True)
 
 # Print a specific review
-print(f"{len(pos_reviews)} positive reviews in total")
-print(pos_reviews['13_7.txt'])
+print(f"{len(preprocessed_pos)} positive reviews in total")
+print(preprocessed_pos['13_7.txt'])
 
 # Generate 3 sets of features 
 # 1: stopwords, lemmatization, TFIDF
 # 2: stopwords, stemming, TFIDF
-# 3: stopwords, lemmatiazation
-# 4: CONSIDER OTHER SETS OF FEATURES (e.g. no stopwords, no punctuation)
+# 3: stopwords, lemmatization
+# 4: stopwords, punctuation, lemmatization
+
+# Apply n-gram after on features set
